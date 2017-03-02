58b91609e16321a6f95bcd61e9c85892281a06a5
Syed Haider Hassan <pakistanihaider@gmail.com>
2017-03-02 12:37:22 +0500

    Added More Tables and MIgrations..

---
+++ app/Http/Controllers/Admin/ChildController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ChildController extends Controller
+{
+
+    //List Down all the Children
+    public function list_children(){
+        
+    }
+}
